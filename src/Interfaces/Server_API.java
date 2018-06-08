c92e3a356a9401928ab004a25140a4fda284a540
rokot108 <38143263+rokot108@users.noreply.github.com>
2018-06-08 18:15:54 +0300

    tmp

--- src/Interfaces/Server_API.java
+++ src/Interfaces/Server_API.java
@@ -5,12 +5,9 @@ public interface Server_API {
     String CLOSE_CONNECTION = "/end";
     String AUTH = "/auth";
     String AUTH_SUCCESSFUL = "/authok";
-<<<<<<< HEAD:src/Interfaces/Server_API.java
     String FILE_REQUEST = "/freq";
     String STRING_SPLITTER = ";";
     String FILE_NOT_FOUND = "/not_found";
-=======
     String PRIVATE_MSG = "/w";
     String USERS_LIST = "/client_list";
->>>>>>> parent of 0ee49a4... Now server app may recieve and write files.:src/Evgeniy/Volkov/Server_API.java
 }
