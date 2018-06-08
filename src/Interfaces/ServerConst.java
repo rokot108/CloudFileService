a9a71c1d7686645bb52d0fa55d1186011269ee92
rokot108 <38143263+rokot108@users.noreply.github.com>
2018-06-08 18:22:03 +0300

    Code Improvements.

--- src/Interfaces/ServerConst.java
+++ src/Interfaces/ServerConst.java
@@ -3,4 +3,7 @@ package Interfaces;
 public interface ServerConst {
     int PORT = 8189;
     String SERVER_URL = "localhost";
+    String HOME_PATH = "C:/CloudService";
+    String CLIENT_PATH = "C:/CloudService/ClientDir";
+    String SERVER_PAPH = "C:/CloudService/ServerDir";
 }
