5a76c8b979485be1513965143e7959a024d7e154
rokot108 <38143263+rokot108@users.noreply.github.com>
2018-06-28 19:44:51 +0300

    Window development continue...

--- src/Client/ClientWindow.java
+++ src/Client/ClientWindow.java
@@ -20,13 +20,14 @@ public class ClientWindow extends JFrame {
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         setSize(500, 500);
         setLocationRelativeTo(null);
-        setMinimumSize(new Dimension(400,400));
+        setMinimumSize(new Dimension(400, 400));
 
-        GridBagLayout gbl = new GridBagLayout();
-        setLayout(gbl);
+        GridBagLayout gbAll = new GridBagLayout();
+        setLayout(gbAll);
         GridBagConstraints c = new GridBagConstraints();
 
-        jPanelTop = new JPanel(new GridBagLayout());
+        GridBagLayout gbTop = new GridBagLayout();
+        jPanelTop = new JPanel(gbTop);
         jPanelTop.setBackground(Color.cyan);
 
         c.anchor = GridBagConstraints.NORTH;
@@ -41,13 +42,13 @@ public class ClientWindow extends JFrame {
         c.weightx = 1;
         c.weighty = 0.6;
 
-        gbl.setConstraints(jPanelTop, c);
+        gbAll.setConstraints(jPanelTop, c);
         add(jPanelTop);
 
         jPanelBottom = new JPanel(new GridBagLayout());
         jPanelBottom.setBackground(Color.BLACK);
 
-        c.anchor = GridBagConstraints.NORTH;
+        c.anchor = GridBagConstraints.SOUTH;
         c.fill = GridBagConstraints.BOTH;
         c.gridheight = 1;
         c.gridwidth = 1;
@@ -59,29 +60,70 @@ public class ClientWindow extends JFrame {
         c.weightx = 1;
         c.weighty = 0.3;
 
-        gbl.setConstraints(jPanelBottom, c);
+        gbAll.setConstraints(jPanelBottom, c);
         add(jPanelBottom);
 
-      /*  jPanelTopLeft = new JPanel(new BorderLayout());
-        jPanelTopLeft.setPreferredSize(new Dimension(170, 210));
+        GridBagLayout gbTopLeft = new GridBagLayout();
+        jPanelTopLeft = new JPanel(gbTopLeft);
         jPanelTopLeft.setBackground(Color.GREEN);
-        jPanelTop.add(jPanelTopLeft, BorderLayout.WEST);
 
-        jPanelTopRight = new JPanel(new BorderLayout());
-        jPanelTopRight.setPreferredSize(new Dimension(170, 210));
+        c.anchor = GridBagConstraints.WEST;
+        c.fill = GridBagConstraints.BOTH;
+        c.gridheight = 1;
+        c.gridwidth = 1;
+        c.gridx = 0;
+        c.gridy = 0;
+        c.insets = new Insets(0, 0, 0, 2);
+        c.ipadx = 0;
+        c.ipady = 0;
+        c.weightx = 0.45;
+        c.weighty = 1;
+
+        gbTop.setConstraints(jPanelTopLeft, c);
+        jPanelTop.add(jPanelTopLeft);
+
+        GridBagLayout gbTopRight = new GridBagLayout();
+        jPanelTopRight = new JPanel(gbTopRight);
         jPanelTopRight.setBackground(Color.RED);
-        jPanelTop.add(jPanelTopRight, BorderLayout.EAST);
 
-        jPanelTopCenter = new JPanel(new GridLayout());
-        jPanelTopCenter.setPreferredSize(new Dimension(60, 210));
-        jPanelTopCenter.setBackground(Color.BLUE);
-        jPanelTop.add(jPanelTopCenter, BorderLayout.CENTER);
+        c.anchor = GridBagConstraints.EAST;
+        c.fill = GridBagConstraints.BOTH;
+        c.gridheight = 1;
+        c.gridwidth = 1;
+        c.gridx = 2;
+        c.gridy = 0;
+        c.insets = new Insets(0, 2, 0, 0);
+        c.ipadx = 0;
+        c.ipady = 0;
+        c.weightx = 0.45;
+        c.weighty = 1;
 
-        add(jPanelTop, BorderLayout.NORTH);*/
+        gbTop.setConstraints(jPanelTopRight, c);
+        jPanelTop.add(jPanelTopRight);
+        jPanelTopRight.setVisible(true);
 
+        GridBagLayout gbTopCenter = new GridBagLayout();
+        jPanelTopCenter = new JPanel(gbTopCenter);
+        jPanelTopCenter.setBackground(Color.YELLOW);
 
-        jPanelTop.setVisible(true);
-        jPanelBottom.setVisible(true);
+        c.anchor = GridBagConstraints.CENTER;
+        c.fill = GridBagConstraints.BOTH;
+        c.gridheight = 1;
+        c.gridwidth = 1;
+        c.gridx = 1;
+        c.gridy = 0;
+        c.insets = new Insets(5, 2, 5, 2);
+        c.ipadx = 0;
+        c.ipady = 0;
+        c.weightx = 0.1;
+        c.weighty = 1;
+
+        gbTop.setConstraints(jPanelTopCenter, c);
+        jPanelTop.add(jPanelTopCenter);
+        jPanelTopCenter.setVisible(true);
+
+        /*jPanelTop.setVisible(true);
+        jPanelBottom.setVisible(true);*/
         setVisible(true);
     }
 }
